466143b7560116acd6a61e2c4912785c4b8667d0
npm8 <nnnp.dawid@gmail.com>
2020-03-24 17:26:30 +0100

    Clean code
    Login function was little bit messed up and here is some basic code split
    clean up with change in initialization flow and use only one patter of
    Promise handling.

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,5 @@
-import tough from 'tough-cookie';
-import qs from 'querystring';
+import tough, { CookieJar } from 'tough-cookie';
+import { stringify } from 'querystring';
 import cheerio from 'cheerio';
 
 const axios = require('axios').default;
@@ -7,70 +7,76 @@ const axiosCookieJarSupport = require('axios-cookiejar-support').default;
 
 export class Client {
   public symbol: string;
+
   public userList: object;
 
-  public async login(username: string, password: string, host: string): Promise<string> {
-    return new Promise(async (resolve, reject) => {
-      axiosCookieJarSupport(axios);
-      const cookieJar = new tough.CookieJar();
-      axios.post(
-        `https://cufs.${host}/Default/Account/LogOn?ReturnUrl=%2FDefault%2FFS%2FLS%3Fwa%3Dwsignin1.0%26wtrealm%3Dhttps%253a%252f%252fuonetplus.${host}%252fDefault%252fLoginEndpoint.aspx%26wctx%3Dhttps%253a%252f%252fuonetplus.${host}%252fDefault%252fLoginEndpoint.aspx`,
-        qs.stringify({
+  public constructor(host: string) {
+    this.host = host;
+    axiosCookieJarSupport(axios);
+    this.cookieJar = new tough.CookieJar();
+  }
+
+  public login(username: string, password: string): Promise<string> {
+    return new Promise(async (resolve) => {
+      const config = {
+        jar: this.cookieJar,
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      };
+
+      const response = await axios.post<string>(
+        this.loginUrl(this.host),
+        stringify({
           LoginName: username,
           Password: password,
         }),
-        {
-          jar: cookieJar,
-          withCredentials: true,
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-        },
-      ).then((response) => {
-        const $ = cheerio.load(response.data);
-        if ($('.ErrorMessage').length) {
-          reject(new Error($('.ErrorMessage').text().trim()));
-          return;
-        }
-        const xml = $('[name=hiddenform]').find('[name=wresult]').attr('value');
-        const x = cheerio.load(xml, {
-          xmlMode: true,
-        });
-        const symbols: string[] = [];
-        x('[AttributeName$="UserInstance"] saml\\:AttributeValue').each(function a(i) {
-          symbols[i] = x(this).text();
-        });
-        if (symbols.length === 0) {
-          reject(new Error('Nie znaleziono symbolu. Zaloguj się, podając dodatkowo symbol.'));
-          return;
-        }
-        for (let i = 0; i < symbols.length; i++) {
-          const symbol = symbols[i];
-          axios.post(
-            `https://uonetplus.${host}/${symbol}/LoginEndpoint.aspx`,
-            qs.stringify({
-              wa: 'wsignin1.0',
-              wresult: xml,
-              wctx: `https://uonetplus.${host}/${symbol}/LoginEndpoint.aspx`,
-            }),
-            {
-              jar: cookieJar,
-              withCredentials: true,
-              headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-              },
-            },
+        config,
+      );
 
-          ).then((responselog) => {
-            if (responselog.data.includes('appLink')) {
-              this.initClient(responselog.data, symbol);
-              resolve(symbol);
-            }
-          });
+      const xml = this.parseLoginResponds(response.data);
+
+      const symbols = this.parseSymbolsXml(xml);
+
+      await Promise.all(symbols.map(async (symbol) => {
+        const { data } = await axios.post(
+          `https://uonetplus.${this.host}/${symbol}/LoginEndpoint.aspx`,
+          stringify({
+            wa: 'wsignin1.0',
+            wresult: xml,
+            wctx: `https://uonetplus.${this.host}/${symbol}/LoginEndpoint.aspx`,
+          }),
+          config,
+        );
+
+        if (data.includes('appLink')) {
+          this.initClient(data, symbol);
+          resolve(symbol);
         }
-      });
+      }));
     });
   }
+
+  /**
+   * Login URL.
+   * @param host Host address.
+   * @return Login endpoint URL.
+   */
+  private loginUrl(host): string {
+    return `https://cufs.${host}/Default/Account/LogOn?ReturnUrl=%2FDefault%2FFS%2FLS%3Fwa%3Dwsignin1.0%26wtrealm%3Dhttps%253a%252f%252fuonetplus.${host}%252fDefault%252fLoginEndpoint.aspx%26wctx%3Dhttps%253a%252f%252fuonetplus.${host}%252fDefault%252fLoginEndpoint.aspx`;
+  }
+
+
+  /**
+   * Client host.
+   */
+  private readonly host: string = '';
+
+  /**
+   * Cookie jar client object.
+   */
+  private readonly cookieJar: CookieJar;
 
   private async initClient(mainResponse: string, symbol: string): Promise<boolean> {
     this.symbol = symbol;
@@ -81,5 +87,44 @@ export class Client {
       links.push(link);
     });
     return true;
+  }
+
+  /**
+   * Parsing login respond to find `wresult` in hidden form in respond from UONET API.
+   * @param html HTML respond data to parse.
+   * @return XML data.
+   */
+  private parseLoginResponds(html: string): string {
+    const $ = cheerio.load(html);
+    const errorMessage: Cheerio = $('.ErrorMessage');
+
+    if (errorMessage.length) {
+      throw new Error(errorMessage.text().trim());
+    }
+
+    return $('[name=hiddenform]').find('[name=wresult]').attr('value');
+  }
+
+  /**
+   * Extract Array with symbols.
+   * @param xml
+   * @returns Array of symbols.
+   */
+  private parseSymbolsXml(xml): string[] {
+    const $ = cheerio.load(xml, {
+      xmlMode: true,
+    });
+
+    const symbols: string[] = $('[AttributeName$="UserInstance"] saml\\:AttributeValue')
+      .map((
+        iterator,
+        element,
+      ) => $(element).text()).get();
+
+    if (symbols.length === 0) {
+      throw new Error('Nie znaleziono symbolu. Zaloguj się, podając dodatkowo symbol.');
+    }
+
+    return symbols;
   }
 }
