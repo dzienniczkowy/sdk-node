3456808b225f51c9bff675f6ea535cc769067a1f
Dominik Korsa <dominik.korsa@gmail.com>
2020-03-25 23:09:28 +0100

    Reformat tests

--- tests/client.e2e.ts
+++ tests/client.e2e.ts
@@ -4,14 +4,12 @@ const client = new wulkanowy.Client('fakelog.cf');
 
 describe('Client', () => {
   describe('Login method', () => {
-    it(
-      'Login to fakelog account',
-      () => expect(client.login('jan@fakelog.cf', 'jan123')).resolves.toEqual('powiatwulkanowy'),
-    );
+    it('Login to fakelog account', () => expect(
+      client.login('jan@fakelog.cf', 'jan123'),
+    ).resolves.toEqual('powiatwulkanowy'));
 
-    it(
-      'Login to fakelog with invalid credentials',
-      async () => expect(client.login('jan@fakelog.cf', 'invalid-password')).rejects.toHaveProperty('name', 'InvalidCredentialsError'),
-    );
+    it('Throws error if login credentials are invalid', () => expect(
+      client.login('jan@fakelog.cf', 'invalid-password'),
+    ).rejects.toHaveProperty('name', 'InvalidCredentialsError'));
   });
 });
