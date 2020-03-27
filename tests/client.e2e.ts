1eca4cabe3b4d79374ccb34e8bbda2495793da32
Dominik Korsa <dominik.korsa@gmail.com>
2020-03-27 10:26:53 +0100

    Add custom error classes (#12)
    * Add invalid credentials and unknown symbol error classes
    
    * Change === to includes
    
    * Reformat tests
    
    * Separate client for each test

--- tests/client.e2e.ts
+++ tests/client.e2e.ts
@@ -1,9 +1,23 @@
 import * as wulkanowy from '../src';
 
-const client = new wulkanowy.Client('fakelog.cf');
-
 describe('Client', () => {
   describe('Login method', () => {
-    it('Login to fakelog account', () => expect(client.login('jan@fakelog.cf', 'jan123')).resolves.toEqual('powiatwulkanowy'));
+    let client;
+
+    beforeEach(() => {
+      client = new wulkanowy.Client('fakelog.cf');
+    });
+
+    afterEach(() => {
+      // TODO: Logout
+    });
+
+    it('Login to fakelog account', () => expect(
+      client.login('jan@fakelog.cf', 'jan123'),
+    ).resolves.toEqual('powiatwulkanowy'));
+
+    it('Throws error if login credentials are invalid', () => expect(
+      client.login('jan@fakelog.cf', 'invalid-password'),
+    ).rejects.toHaveProperty('name', 'InvalidCredentialsError'));
   });
 });
