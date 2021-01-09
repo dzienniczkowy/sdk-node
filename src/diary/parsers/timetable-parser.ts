a37657f686b1d9d8f6aef98aed1e7a6a588994d3
Dominik Korsa <dominik.korsa@gmail.com>
2021-01-09 13:37:43 +0100

    Reorder functions in timetable-parser.ts

--- src/diary/parsers/timetable-parser.ts
+++ src/diary/parsers/timetable-parser.ts
@@ -4,6 +4,76 @@ import { notNil } from '../../utils';
 import { TimetableData } from '../interfaces/timetable/timetable-data';
 import { TimetableLesson, TimetableLessonInfo } from '../interfaces/timetable/timetable-lesson';
 
+function getSubjectAndGroupFromSpan(span: Cheerio): { subject: string; group?: string } {
+  return {
+    subject: span.text().substr(
+      0,
+      span.text().includes(' [') ? span.text().indexOf(' [') : span.text().length,
+    ),
+    group: span.text().includes(' [') ? span.text().substring(span.text().indexOf('[') + 1, span.text().indexOf(']')) : undefined,
+  };
+}
+
+function getFormattedLessonInfo(info: string): string {
+  return info.substring(1, info.length - 1);
+}
+
+function getLesson(spans: Cheerio, offset = 0): TimetableLessonInfo {
+  const { subject, group } = getSubjectAndGroupFromSpan(spans.eq(0));
+  return {
+    subject,
+    group,
+    teacher: spans.eq(1 + offset).text(),
+    room: spans.eq(2 + offset).text(),
+    info: 3 + offset < spans.length
+      ? getFormattedLessonInfo(spans.eq(3 + offset).text())
+      : undefined,
+    canceled: spans.hasClass('x-treelabel-inv'),
+    hasChanges: spans.hasClass('x-treelabel-zas'),
+  };
+}
+
+function getLessonWithReplacement(spans: Cheerio, offset = 0): TimetableLessonInfo {
+  const { subject, group } = getSubjectAndGroupFromSpan(spans.eq(3 + offset));
+  const { subject: subjectOld } = getSubjectAndGroupFromSpan(spans.eq(0));
+  return {
+    subject,
+    subjectOld,
+    group,
+    teacher: spans.eq(4 + offset * 2).text(),
+    teacherOld: spans.eq(1 + offset).text(),
+    room: spans.eq(5 + offset * 2).text(),
+    roomOld: spans.eq(2 + offset).text(),
+    info: `${getFormattedLessonInfo(spans.last().text())}, poprzednio: ${subjectOld}`,
+    hasChanges: true,
+    canceled: false,
+  };
+}
+
+function getLessonInfo(div: string): TimetableLessonInfo | null {
+  const $ = cheerio.load(div);
+  const spans = $('span');
+  if (spans.length === 3) return getLesson(spans);
+  if (spans.length === 4 && spans.last().hasClass('x-treelabel-rlz')) return getLesson(spans);
+  if (spans.length === 4) return getLesson(spans, 1);
+  if (spans.length === 5 && spans.last().hasClass('x-treelabel-rlz')) return getLesson(spans, 1);
+  if (spans.length === 7) return getLessonWithReplacement(spans);
+  if (spans.length === 9) return getLessonWithReplacement(spans, 1);
+  return null;
+}
+
+function stripLessonInfo(info: string): string {
+  const newInfo = info.startsWith(', ') ? info.substring(2, info.length) : info;
+  return newInfo
+    .replace('okienko dla uczniów', '')
+    .replace('zmiana organizacji zajęć', '')
+    .replace(' ,', '');
+}
+
+function capitalizeString(s: string): string {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
 function parseCell(cell: string): TimetableLessonInfo | null {
   const $ = cheerio.load(cell);
   const divs = $('div:not([class])');
@@ -90,76 +160,6 @@ function parseCell(cell: string): TimetableLessonInfo | null {
     }
     return getLessonInfo(divs.eq(0).html()!);
   } return null;
-}
-
-function getLessonInfo(div: string): TimetableLessonInfo | null {
-  const $ = cheerio.load(div);
-  const spans = $('span');
-  if (spans.length === 3) return getLesson(spans);
-  if (spans.length === 4 && spans.last().hasClass('x-treelabel-rlz')) return getLesson(spans);
-  if (spans.length === 4) return getLesson(spans, 1);
-  if (spans.length === 5 && spans.last().hasClass('x-treelabel-rlz')) return getLesson(spans, 1);
-  if (spans.length === 7) return getLessonWithReplacement(spans);
-  if (spans.length === 9) return getLessonWithReplacement(spans, 1);
-  return null;
-}
-
-function getLesson(spans: Cheerio, offset = 0): TimetableLessonInfo {
-  const { subject, group } = getSubjectAndGroupFromSpan(spans.eq(0));
-  return {
-    subject,
-    group,
-    teacher: spans.eq(1 + offset).text(),
-    room: spans.eq(2 + offset).text(),
-    info: 3 + offset < spans.length
-      ? getFormattedLessonInfo(spans.eq(3 + offset).text())
-      : undefined,
-    canceled: spans.hasClass('x-treelabel-inv'),
-    hasChanges: spans.hasClass('x-treelabel-zas'),
-  };
-}
-
-function getLessonWithReplacement(spans: Cheerio, offset = 0): TimetableLessonInfo {
-  const { subject, group } = getSubjectAndGroupFromSpan(spans.eq(3 + offset));
-  const { subject: subjectOld } = getSubjectAndGroupFromSpan(spans.eq(0));
-  return {
-    subject,
-    subjectOld,
-    group,
-    teacher: spans.eq(4 + offset * 2).text(),
-    teacherOld: spans.eq(1 + offset).text(),
-    room: spans.eq(5 + offset * 2).text(),
-    roomOld: spans.eq(2 + offset).text(),
-    info: `${getFormattedLessonInfo(spans.last().text())}, poprzednio: ${subjectOld}`,
-    hasChanges: true,
-    canceled: false,
-  };
-}
-
-function getSubjectAndGroupFromSpan(span: Cheerio): { subject: string; group?: string } {
-  return {
-    subject: span.text().substr(
-      0,
-      span.text().includes(' [') ? span.text().indexOf(' [') : span.text().length,
-    ),
-    group: span.text().includes(' [') ? span.text().substring(span.text().indexOf('[') + 1, span.text().indexOf(']')) : undefined,
-  };
-}
-
-function getFormattedLessonInfo(info: string): string {
-  return info.substring(1, info.length - 1);
-}
-
-function stripLessonInfo(info: string): string {
-  const newInfo = info.startsWith(', ') ? info.substring(2, info.length) : info;
-  return newInfo
-    .replace('okienko dla uczniów', '')
-    .replace('zmiana organizacji zajęć', '')
-    .replace(' ,', '');
-}
-
-function capitalizeString(s: string): string {
-  return s.charAt(0).toUpperCase() + s.slice(1);
 }
 
 export function parseTimetable(htmlResponse: TimetableData): TimetableLesson[] {
