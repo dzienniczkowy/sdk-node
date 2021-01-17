610b83a8aba1fc4dfa079a291f6cb66c474890cf
Dominik Korsa <dominik.korsa@gmail.com>
2021-01-17 21:23:05 +0100

    Make RequestFailedError.response public

--- src/errors/request-not-successful.ts
+++ src/errors/request-not-successful.ts
@@ -2,7 +2,7 @@ import type { AxiosResponse } from 'axios';
 
 // TODO: Add fields for error details
 export default class RequestFailedError extends Error {
-  constructor(private response: AxiosResponse) {
+  constructor(public response: AxiosResponse) {
     super('Request failed');
     this.name = 'RequestFailedError';
   }
